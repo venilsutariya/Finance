4e511b7460cdc7185d930b54a09d07c934556fa6
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2024-07-22 10:39:26 +0530

    complete 1h

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { string, z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 export const runtime = "edge";
 
@@ -11,14 +13,45 @@ app
       message: "Hello Next.js!",
     });
   })
-  .get("/hello/:id", (c) => {
-    const id = c.req.param("id");
+  .get(
+    "/hello/:id",
+    zValidator(
+      "param",
+      z.object({
+        id: z.string(),
+      })
+    ),
+    (c) => {
+      const { id } = c.req.valid("param");
 
-    return c.json({
-      message: "Hello Next.js!",
-      id,
-    });
-  });
+      return c.json({
+        message: "Hello Next.js!",
+        id,
+      });
+    }
+  )
+  .post(
+    "/create/:postId",
+    zValidator(
+      "json",
+      z.object({
+        name: z.string(),
+        userId: z.number(),
+      })
+    ),
+    zValidator(
+      "param",
+      z.object({
+        postId: z.number(),
+      })
+    ),
+    (c) => {
+      const { name, userId } = c.req.valid("json");
+      const { postId } = c.req.valid("param");
+
+      return c.json({});
+    }
+  );
 
 export const GET = handle(app);
 export const POST = handle(app);
